884557569ac8eccfdd73bc4267dafdf768b2b593
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2018-12-20 12:28:52 +0200

    question twelve

--- javascript.js
+++ javascript.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
   $("#hide").click(function() {
     $("#submit").hide(1000);
   });
-  $("#nextQuestion").click(function() {
-    $("#next").next(1000);
-  });
 
   var beverage = $("#beverage").val();
   var flavor = $("input:radio[name=one]:checked").val();
