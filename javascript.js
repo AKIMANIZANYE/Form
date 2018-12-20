99a84ae79f6fa7a0e7ca2ffbdee7fb37b1ddc0b4
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2018-12-20 10:24:38 +0200

    I added the haeding four

--- javascript.js
+++ javascript.js
@@ -1,2 +1,8 @@
-var beverage = $("#beverage").val();
-var flavor = $("input:radio[name=flavor]:checked").val();
+$(document).ready(function() {
+  $("#hide").click(function() {
+    $("#submit").hide(1000);
+  });
+
+  var beverage = $("#beverage").val();
+  var flavor = $("input:radio[name=flavor]:checked").val();
+});
