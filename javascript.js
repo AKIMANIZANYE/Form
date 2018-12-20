546a248175c7482c90e81329f2858338d88c27e8
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2018-12-20 13:39:57 +0200

    background color

--- javascript.js
+++ javascript.js
@@ -3,6 +3,26 @@ $(document).ready(function() {
     $("#submit").hide(1000);
   });
 
+  $("#blanks form").submit(function(event) {
+    var blanks = [
+      "person1",
+      "person2",
+      "animal",
+      "exclamation",
+      "verb",
+      "noun"
+    ];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+
   var beverage = $("#beverage").val();
   var flavor = $("input:radio[name=one]:checked").val();
 });
