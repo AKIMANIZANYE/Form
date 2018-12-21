669fa9c04d838bf4931208bdc52c931974611782
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2018-12-21 08:24:45 +0200

    th description

---
+++ README.md
@@ -0,0 +1,5 @@
+#Project name.: Quiz Board
+Project description.
+My project called is called Quiz Board it is in kind of form , I design it so that
+
+Author(s) information. #Setup instructions - includes any scripts that need to be run if necessary. Firstly i declared the array in my vajascript I used if statment in javascript in addition I used also ifesle statments I used sytles in CSS and i used heading ,links i used also paragraph in HTMLs code not only that but also, I added i used input buttom that allows the users to enter the number that they wish to enter. #Author: Claudine Akimanizanye akimanizanye.claudine@gmail.com Date: Fri Dec 14 11:50:41 2018 +0200
--- javascript.js
+++ javascript.js
@@ -3,26 +3,94 @@ $(document).ready(function() {
     $("#submit").hide(1000);
   });
 
-  $("#blanks form").submit(function(event) {
-    var blanks = [
-      "person1",
-      "person2",
-      "animal",
-      "exclamation",
-      "verb",
-      "noun"
-    ];
+  $(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+      var blanks = ["name", "secondName", "yourId", "email"];
 
-    blanks.forEach(function(blank) {
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
+      blanks.forEach(function(blank) {
+        var userInput = $("input#" + blank).val();
+        $("." + blank).text(userInput);
+      });
+
+      $("#story").show();
+
+      event.preventDefault();
     });
-
-    $("#story").show();
-
-    event.preventDefault();
   });
 
   var beverage = $("#beverage").val();
   var flavor = $("input:radio[name=one]:checked").val();
 });
+function check() {
+  var qOne = document.test.one.value;
+  var qTwo = document.test.two.value;
+  var qThree = document.test.three.value;
+  var qFour = document.test.four.value;
+  var qFive = document.test.five.value;
+  var qSix = document.test.six.value;
+  var qSeven = document.test.seven.value;
+  var qEight = document.test.eight.value;
+  var qNine = document.test.nine.value;
+  var qTen = document.test.ten.value;
+  var qEleven = document.test.eleven.value;
+  var qTwelve = document.test.twelve.value;
+  var qThirteen = document.test.thirteen.value;
+  var qFourteen = document.test.fourteen.value;
+  var count = 0;
+
+  if (one == "1") {
+    count += 1;
+  }
+
+  if (qTwo == "2") {
+    count += 1;
+  }
+  if (qThree == "3") {
+    count += 1;
+  }
+  if (qFour == "4") {
+    count += 1;
+  }
+  if (qFive == "5") {
+    count += 1;
+  }
+  if (qSix == "6") {
+    count += 1;
+  }
+  if (qSeven == "7") {
+    count += 1;
+  }
+
+  if (qEight == "8") {
+    count += 1;
+  }
+  if (qNine == "9") {
+    count += 1;
+  }
+  if (qTen == "10") {
+    count += 1;
+  }
+  if (qEleven == "select") {
+    count += 1;
+  }
+  if (qTwelve == "select") {
+    count += 1;
+  }
+
+  if (qThirteen == "select") {
+    count += 1;
+  }
+  if (qFourteen == "select") {
+    count += 1;
+  }
+
+  document.getElementById("after_submit").style.visibility = "visible";
+
+  document.getElementById("number_count").innerHTML =
+    "Your Score is " + count + "14.";
+
+  $("#button").hide();
+  $("#test").hide();
+  $("#number_count").show();
+  event.preventDefault();
+}
